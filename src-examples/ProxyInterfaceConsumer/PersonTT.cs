8e2d774242bc9f0c7c1c4c0770dbf2475213258f
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-08-01 09:24:35 +0200

    TT

--- src-examples/ProxyInterfaceConsumer/PersonTT.cs
+++ src-examples/ProxyInterfaceConsumer/PersonTT.cs
@@ -1,5 +1,18 @@
 ﻿namespace ProxyInterfaceConsumer
 {
+    public class PersonT<T1, T2>
+        where T1 : struct
+        where T2 : class, new()
+    {
+        public T1 TVal1 { get; set; }
+
+        public T2 TVal2 { get; set; }
+
+        public void Call(int x, T1 t1, T2 t2)
+        {
+        }
+    }
+
     public class PersonTT<T1, T2>
         where T1 : struct
         where T2 : class, new()
