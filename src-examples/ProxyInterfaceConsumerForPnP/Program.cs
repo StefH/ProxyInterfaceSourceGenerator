f109fc6e9b59c35904e0ac51d2e4325074b06e92
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-09-04 16:32:29 +0200

    .

--- src-examples/ProxyInterfaceConsumerForPnP/Program.cs
+++ src-examples/ProxyInterfaceConsumerForPnP/Program.cs
@@ -8,7 +8,6 @@ public class Program
     public static void Main()
     {
         var cp = new ClientContextProxy(new ClientContext("https://heyenrath.nl"));
-        //cp.Test();
 
         cp.Load3(cp.Web, w => w.Lists, w => w.Language);
 
