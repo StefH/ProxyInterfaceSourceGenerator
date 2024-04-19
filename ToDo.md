384cd65a73293146211bde9621553d0d444b8d55
David Allwicher <39563238+Fieslix@users.noreply.github.com>
2024-04-19 09:29:29 +0200

    Implemented unit tests for the interface inheritance

---
+++ ToDo.md
@@ -0,0 +1,5 @@
+# Ideas for further improvements
+
+## Replaced out parameters
+## Replaced Events
+## Generate explicit interface code
---
+++ tests/ProxyInterfaceSourceGeneratorTests/Source/Disposable/Explicit.cs
@@ -0,0 +1,25 @@
+namespace ProxyInterfaceSourceGeneratorTests.Source.Disposable
+{
+    public class Explicit : IDisposable, IUpdate<string>
+    {
+        string IUpdate<string>.Name => throw new NotSupportedException();
+
+        event EventHandler<string>? IUpdate<string>.Update
+        {
+            add
+            {
+                throw new NotSupportedException();
+            }
+
+            remove
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
