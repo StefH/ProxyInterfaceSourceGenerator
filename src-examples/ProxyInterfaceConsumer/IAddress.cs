7f31f43f9705079309d4c1c64957d1ee1d9fcdf9
Adam Hathcock <adam@speckle.systems>
2024-05-21 14:18:29 +0100

    add _Instance to interface to test

--- src-examples/ProxyInterfaceConsumer/IAddress.cs
+++ src-examples/ProxyInterfaceConsumer/IAddress.cs
@@ -5,5 +5,9 @@ namespace ProxyInterfaceConsumer
     [Proxy(typeof(Address), false, ProxyClassAccessibility.Public, new []{"Weird"})]
     public partial interface IAddress
     {
+        public void Weird()
+        {
+            _Instance.Weird2();
+        }
     }
 }
