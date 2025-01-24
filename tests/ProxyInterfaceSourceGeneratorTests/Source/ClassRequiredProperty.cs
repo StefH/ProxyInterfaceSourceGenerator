485f7a657b2a8c001a43870398073c052784cfc6
Stef <stef.heyenrath@gmail.com>
2025-01-24 20:37:19 +0100

    public required string Test2 { get; init; }

--- tests/ProxyInterfaceSourceGeneratorTests/Source/ClassRequiredProperty.cs
+++ tests/ProxyInterfaceSourceGeneratorTests/Source/ClassRequiredProperty.cs
@@ -3,5 +3,7 @@ namespace ProxyInterfaceSourceGeneratorTests.Source
     public class ClassRequiredProperty
     {
         public required string Test { get; set; }
+
+        public required string Test2 { get; init; }
     }
 }
