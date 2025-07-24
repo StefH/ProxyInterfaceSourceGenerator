68d1ba45d80a3a75aa229f6ae2e04766326f2c8a
Stef <stef.heyenrath@gmail.com>
2025-07-24 13:09:28 +0200

    dispose

--- src/ProxyInterfaceSourceGenerator.Tool/CSharpSimplifier.cs
+++ src/ProxyInterfaceSourceGenerator.Tool/CSharpSimplifier.cs
@@ -5,27 +5,20 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace ProxyInterfaceSourceGenerator.Tool;
 
-public class CSharpSimplifier
+public class CSharpSimplifier(HashSet<MetadataReference> references)
 {
-    private readonly HashSet<MetadataReference> _references;
-
-    public CSharpSimplifier(HashSet<MetadataReference> references)
-    {
-        _references = references;
-    }
-
     public async Task<string> SimplifyCSharpCodeAsync(string sourceCode)
     {
         var id = Guid.NewGuid().ToString("N");
 
-        var workspace = new AdhocWorkspace();
+        using var workspace = new AdhocWorkspace();
         workspace.Options
             .WithChangedOption(FormattingOptions.IndentationSize, LanguageNames.CSharp, 2);           
 
         var project = workspace
             .CurrentSolution
             .AddProject(id, $"{id}.dll", LanguageNames.CSharp)
-            .WithMetadataReferences(_references);
+            .WithMetadataReferences(references);
 
         var document = project.AddDocument($"Input_{id}.cs", SourceText.From(sourceCode));
 
@@ -37,10 +30,8 @@ public class CSharpSimplifier
 
         var simplifiedDoc = await Simplifier.ReduceAsync(newDoc, workspace.Options);
 
-        var formattedDoc = await Formatter.FormatAsync(simplifiedDoc, workspace.Options);
-
-        var simplifiedCode = (await formattedDoc.GetTextAsync()).ToString();
-
-        return simplifiedCode;
+        // var formattedDoc = await Formatter.FormatAsync(simplifiedDoc, workspace.Options);
+        
+        return (await simplifiedDoc.GetTextAsync()).ToString();
     }
 }
